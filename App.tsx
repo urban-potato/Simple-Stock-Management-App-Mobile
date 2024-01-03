122d90275662537026c4ce8d0e2cdc6fcd44ce49
Urban Potato <veryprogramming@gmail.com>
2024-01-04 01:03:23 +0700

    Preparations

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
-// import { StyleSheet, Text, View } from 'react-native'
 import { Text, View } from 'react-native'
 
 export default function App() {
 	return (
-		// <View style={styles.container}>
 		<View className='flex-1 items-center justify-center bg-white'>
 			<Text className='text-green-500'>
 				Open up App.tsx to start working on your app!
@@ -13,12 +11,3 @@ export default function App() {
 		</View>
 	)
 }
-
-// const styles = StyleSheet.create({
-// 	container: {
-// 		flex: 1,
-// 		backgroundColor: '#fff',
-// 		alignItems: 'center',
-// 		justifyContent: 'center'
-// 	}
-// })
